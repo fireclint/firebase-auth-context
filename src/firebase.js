a1b9a2944cdec1e57d4e4c3191eec93ff210e097
Clint Briley <firebriley@Clints-MBP.home>
2022-04-09 18:08:16 -0500

    firebase-auth-context

--- src/firebase.js
+++ src/firebase.js
@@ -7,15 +7,18 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR UNIQUE API KEY",
-  authDomain: "YOUR UNIQUE AUTHDOMAIN",
-  projectId: "YOUR UNIQUE PROJECTID",
-  storageBucket: "YOUR UNIQUE STORAGEBUCKET",
-  messagingSenderId: "YOUR UNIQUE MESSAGINGSENDERID",
-  appId: "YOUR UNIQUE API KEY"
+apiKey: "YOUR UNIQUE API KEY",
+authDomain: "YOUR UNIQUE AUTHDOMAIN",
+projectId: "YOUR UNIQUE PROJECTID",
+storageBucket: "YOUR UNIQUE STORAGEBUCKET",
+messagingSenderId: "YOUR UNIQUE MESSAGINGSENDERID",
+appId: "YOUR UNIQUE API KEY"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app
+
+
+
